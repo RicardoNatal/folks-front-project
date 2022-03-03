2ff9af7206af155e075bbb91a8a39d06d368a284
Ricardo Schvambach <Ricardo.Schvambach@senior.com.br>
2022-03-03 13:42:42 -0300

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { Product } from './product';
+import { ProductService } from './productservice';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'folks-front-project';
